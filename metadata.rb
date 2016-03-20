d5bb83874e1782b14d86e8499e3a852e6612867c
Kevin Dickerson <kevin.dickerson@loom.technology>
2016-03-19 20:41:19 -0700

    Update short description

--- metadata.rb
+++ metadata.rb
@@ -2,8 +2,8 @@ name 'example'
 maintainer 'Kevin Dickerson'
 maintainer_email 'kevin.dickerson@loom.technology'
 license 'Apache 2.0'
-description 'Recipe cookbook consuming Chef resources defined in a ' \
-            'dependency. Tests in InSpec.'
+description 'Recipe cookbook that calls Chef resources defined in a separate ' \
+            'resource cookbook dependency. Tests in InSpec.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.4'
 
